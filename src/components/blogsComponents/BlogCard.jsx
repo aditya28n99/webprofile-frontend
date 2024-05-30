394b3221056e0594b295effed1f36265e03aa1ef
Aditya Sunil Wakale <adityawakale208@gmail.com>
2024-05-31 02:21:53 +0530

    blogs page with blog card added

---
+++ src/components/blogsComponents/BlogCard.jsx
@@ -0,0 +1,54 @@
+// frontend/src/components/blogsComponents/BlogCard.jsx
+import React from 'react';
+import { Link } from 'react-router-dom';
+import styled from 'styled-components';
+
+const Card = styled.div`
+  border: 1px solid #ddd;
+  border-radius: 8px;
+  padding: 16px;
+  margin: 16px;
+  max-width: 300px;
+  transition: transform 0.2s;
+  
+  &:hover {
+    transform: scale(1.05);
+  }
+`;
+
+const Image = styled.img`
+  width: 100%;
+  height: 200px;
+  object-fit: cover;
+  border-radius: 8px;
+`;
+
+const Title = styled.h2`
+  font-size: 1.5em;
+  margin: 8px 0;
+`;
+
+const Date = styled.p`
+  color: lightgray;
+  font-size: 0.9em;
+`;
+
+const Description = styled.p`
+  font-size: 1em;
+  color: #333;
+`;
+
+const BlogCard = ({ blog }) => {
+  return (
+    <Link to={`/blogs/${blog.id}`} style={{ textDecoration: 'none', color: 'inherit' }}>
+      <Card>
+        <Image src={blog.image_url} alt={blog.title} />
+        <Title>{blog.title}</Title>
+        <Date>Published on: {blog.date}</Date>
+        <Description>{blog.content.substring(0, 100)}...</Description>
+      </Card>
+    </Link>
+  );
+};
+
+export default BlogCard;
--- src/pages/Blogs.js
+++ src/pages/Blogs.js
@@ -1,7 +1,54 @@
-import React from 'react'
+// frontend/src/pages/blogs.js
+import React, { useEffect, useState } from 'react';
+import axios from 'axios';
+import BlogCard from '../components/blogsComponents/BlogCard.jsx';
+import styled from 'styled-components';
 
-export default function Blogs() {
+  /* Css for projects cards */
+  const ProjectsPage = styled.div`
+  padding: 30px;` 
+  
+  const ProjectsList = styled.div`
+  display: flex;
+    flex-wrap: wrap;
+    justify-content: space-around;
+  `
+
+const Blogs = () => {
+  const [blogs, setBlogs] = useState([]);
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState(null);
+
+  useEffect(() => {
+    axios.get('http://localhost:5000/blogs')
+      .then(response => {
+        setBlogs(response.data);
+        setLoading(false);
+      })
+      .catch(error => {
+        console.error('Error fetching projects:', error);
+        setError(error);
+        setLoading(false);
+      });
+  }, []);
+
   return (
-    <div>Blogs</div>
-  )
-}
+    <ProjectsPage className="projects-page">
+  <h1>My Blogs</h1>
+  {loading ? (
+    <p>Loading...</p>
+  ) : error ? (
+    <p>Error fetching project details: {error.message}</p>
+  ) : (
+    <ProjectsList className="projects-list">
+      {blogs.map(blog => (
+        <BlogCard key={blog.id} blog={blog} />
+      ))}
+    </ProjectsList>
+  )}
+</ProjectsPage>
+
+  );  
+};
+
+export default Blogs;
